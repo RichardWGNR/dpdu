a0db2cbb186df25b32f93848df5442083fd39ea4
RichardWGNR <171420035+RichardWGNR@users.noreply.github.com>
2026-07-14 04:24:02 +0500

    [dpdu-wrapper-support] feat: com param stack configurator macro

--- dpdu-wrapper-support/src/lib.rs
+++ dpdu-wrapper-support/src/lib.rs
@@ -1,8 +1,14 @@
 use proc_macro::TokenStream;
 
 mod worker_rpc;
+mod com_param_stack_configurator;
 
 #[proc_macro]
 pub fn declare_worker_rpc(input: TokenStream) -> TokenStream {
     worker_rpc::declare_worker_rpc(input)
+}
+
+#[proc_macro]
+pub fn impl_configure_from_serde_json_map_for_com_param_stack(input: TokenStream) -> TokenStream {
+    com_param_stack_configurator::impl_configure_from_serde_json_map_for_com_param_stack(input)
 }
